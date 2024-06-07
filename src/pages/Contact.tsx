a4789119d8802702d9670eac3b62d4f000487876
anggakswr <angga@primaku.com>
2024-06-07 12:52:46 +0700

    feat: new source code

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -90,47 +90,60 @@ export default function Contact() {
       <p className="mb-4 font-bold">My source codes</p>
 
       <div className="grid gap-4 md:grid-cols-2">
-        <div className="overflow-hidden rounded-xl border">
-          <Image
-            src="/img/source-codes/crud-fullstack.jpg"
-            alt="CRUD Fullstack"
-            radius="none"
-          />
+        {sourceCodes.map((sc) => (
+          <div
+            key={`sc-${sc.link}`}
+            className="overflow-hidden rounded-xl border"
+          >
+            <Image src={sc.img} alt={sc.title} radius="none" />
 
-          <div className="p-4">
-            <p className="mb-1 font-bold">CRUD Fullstack</p>
+            <div className="p-4">
+              <p className="mb-1 font-bold">{sc.title}</p>
 
-            <p className="mb-4 text-sm">
-              CRUD fullstack project using Next.js pages router, TypeScript,
-              Tailwind, NextUI, and react-icons.
-            </p>
+              <p className="mb-4 text-sm">{sc.desc}</p>
 
-            <Button
-              as={`a`}
-              href={`https://t.me/anggakswr`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mr-4 bg-white"
-            >
-              Buy
-            </Button>
+              <Button
+                as={`a`}
+                href={`https://t.me/anggakswr`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mr-4 bg-white"
+              >
+                Buy
+              </Button>
 
-            <Button
-              as={`a`}
-              href="https://crud-fullstack.daudweb.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white"
-              variant="bordered"
-            >
-              View
-            </Button>
+              <Button
+                as={`a`}
+                href={sc.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+                variant="bordered"
+              >
+                View
+              </Button>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
 }
+
+const sourceCodes = [
+  {
+    img: "/img/source-codes/crud-fullstack.jpg",
+    title: "CRUD Fullstack",
+    desc: "CRUD fullstack project using Next.js pages router, TypeScript, Tailwind, NextUI, and react-icons.",
+    link: "https://crud-fullstack.daudweb.com/",
+  },
+  {
+    img: "/img/source-codes/crud-react-js.jpg",
+    title: "CRUD React JS",
+    desc: "CRUD frontend project using React.js, TypeScript, Tailwind, NextUI, Zustand, react-toastify, and lucide-react.",
+    link: "https://crud-react-js.daudweb.com/",
+  },
+];
 
 const gears = [
   {
